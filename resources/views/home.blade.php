91a9701e6d2010ed744781b5ffba182ecce13e33
miriam <miriamwangui525@gmail.com>
2026-05-05 13:12:32 +0300

    changed the loaded banner

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -41,7 +41,7 @@
         width: 100%;
         height: 100%;
         z-index: 0;
-        background-image: url('{{ asset('images/Website Banner.jpeg') }}');
+        background-image: url('{{ asset('images/feedsltd.jpeg') }}');
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
